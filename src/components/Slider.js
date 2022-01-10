a02f91f1eb96388f61e9cbbfa320120ae0d2a349
Songeun Choi <a66350623@gmail.com>
2022-01-10 17:35:35 +0900

    Slider.js 함수형 변환

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,49 +1,36 @@
-import React, { Component } from "react";
+import React from "react";
 import Slider from "react-slick";
 
-const DEMO_PROPS = [
-  {
-    url: "www.naver.com",
-    img: "https://pstatic.naver.com/어쩌구 저쩌구",
-  },
-  {
-    url: "www.naver2.com",
-    img: "https://pstatic.naver2.com/어쩌구 저쩌구2",
-  },
-];
-
-export default class SimpleSlider extends Component {
-  constructor(props) {
-    super(props);
-    this.count = props.count;
-  }
-
-  makeSliderChildren() {
-    return DEMO_PROPS.map((element, index) => {
-      return (
-        <div key={index}>
-          <h3>{element.url}</h3>
-          <h3>{element.img}</h3>
+function SimpleSlider(){
+  
+  function makeSliderChildren(){
+    let retval = [];
+    for (let i = 0; i <6; ++i) {
+      retval.push(
+        <div key={i * Math.random()}>
+          <h3>{i + 1}</h3>
         </div>
       );
-    });
+    }
+    
+    return retval;
   }
 
-  render() {
-    const settings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      autoplay: true,
-    };
-
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
     return (
       <div>
         <h2> Single Item</h2>
-        <Slider {...settings}>{this.makeSliderChildren()}</Slider>
+        <Slider {...settings}>
+          {makeSliderChildren()}
+        </Slider>
       </div>
     );
   }
-}
+
+export default SimpleSlider;
