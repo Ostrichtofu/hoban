a257048c570019544bacf4fc0429a2080f464351
Song Eun Choi <a66350623@gmail.com>
2022-01-18 00:08:42 +0900

    메인페이지 컴포넌트 및 SCSS  작업

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-import React from "react";
+import React, { Component } from "react";
 import "./App.scss";
 import "./scss/Reset.css";
-
-import Header from "./components/Header";
-import MainSlider from "./components/main/MainSlider";
-import Contents from "./components/main/Contents";
-import Footer from "./components/Footer";
-
-function App() {
+import MainPage from "./page/mainPage";
+import { Route, Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
+export default function App() {
   return (
-    <div>
-      <Header />
-      <MainSlider />
-      <Contents />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <Route exact path="/main" component={MainPage} />
+    </BrowserRouter>
   );
 }
-export default App;
